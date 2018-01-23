23baf61e23a48ec2b71f57c2f7bb2e2186151184
ddkino <ddkino-github@yahoo.fr>
2018-01-23 23:57:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,6 +64,9 @@ export PATH=$PATH:$SCALA_HOME/bin
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+#docker machine
+curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && \
+
 #android
 sudo apt-get install android-tools-adb android-tools-fastboot
 
