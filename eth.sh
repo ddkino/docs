c7a1723b5dd32a13e6943e15ae33531be5159d84
ddkino <ddkino-github@yahoo.fr>
2018-03-11 01:56:18 +0100

    Create eth.sh

---
+++ eth.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo add-apt-repository -y ppa:ethereum/ethereum
+
+sudo apt-get update
+sudo apt-get install ethereum
+
+# docker version
+# image : ethereum/client-go:latest
+# ethereum/client-go:alltools-latest
