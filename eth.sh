cd004f429dca3aa36042317e314614b88af9adc0
ddkino <ddkino-github@yahoo.fr>
2018-03-17 16:33:49 +0100

    dev tools : remix truffle

--- eth.sh
+++ eth.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# install geth or go-client locally but create a node for the blockchain
 sudo add-apt-repository -y ppa:ethereum/ethereum
 
 sudo apt-get update
@@ -11,3 +12,25 @@ sudo apt-get install ethereum
 
 
 sudo apt-get install solc
+
+# dev suite 
+yarn global add truffle
+
+
+https://mg.mail.yahoo.com/neo/launch?.rand=7e4nvrgnv91d0#
+
+#remix IDE
+
+#Remix-ide has been published as an npm module:
+
+npm install remix-ide -g
+remix-ide
+
+#Or if you want to clone the github repository:
+
+git clone https://github.com/ethereum/remix-ide.git
+cd remix-ide
+npm install
+npm run build && npm run serve
+
+
