3a78d4d664458512fd959dc0ef5cbdcd0f854f9d
David <david.ddkino@gmail.com>
2020-08-15 13:30:24 +0200

    Update boot_windows.sh

--- boot_windows.sh
+++ boot_windows.sh
@@ -1,3 +1,8 @@
-sudo add-apt-repository ppa:gezakovacs/ppa
-sudo apt-get update
-sudo apt-get install unetbootin
+git clone https://github.com/slacka/WoeUSB.git
+cd WoeUSB/
+./setup-development-environment.bash
+sudo apt-get install devscripts equivs gdebi-core
+mk-build-deps
+sudo gdebi woeusb-build-deps_*.deb
+dpkg-buildpackage -uc -b
+sudo gdebi ../woeusb*.deb
