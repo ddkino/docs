a980d8fc3b2e13cddc7ed826d07e72c974ede0b3
David <david.ddkino@gmail.com>
2021-04-28 20:09:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,10 +62,12 @@ export PATH=$PATH:$SCALA_HOME/bin
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-#docker machine
-curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && \
-chmod +x /tmp/docker-machine && \
-sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+#gitlab runner
+# For Debian/Ubuntu/Mint
+curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | sudo bash
+export GITLAB_RUNNER_DISABLE_SKEL=true; sudo -E apt-get install gitlab-runner
+
+
 #android
 sudo apt-get install android-tools-adb android-tools-fastboot
 
