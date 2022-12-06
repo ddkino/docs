32ce04c320396d2d49d54c732548c6bcf21c13a1
David <david.ddkino@gmail.com>
2022-12-07 01:15:50 +0200

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,7 @@
+# how to install specific python
+
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-cache policy python(version)
+sudo apt-cache policy python3.9
+sudo apt install python3.9
