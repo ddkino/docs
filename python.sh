932246e36f8316bc9e659ecbefffecc34f7ad58a
David <david.ddkino@gmail.com>
2022-12-07 09:43:53 +0200

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,4 +1,5 @@
 # how to install specific python
+# https://www.how2shout.com/linux/install-python-3-9-or-3-8-on-ubuntu-22-04-lts-jammy-jellyfish/
 
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
