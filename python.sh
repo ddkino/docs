dc03042e016a1bbb48c8de64e9d5db920fae4830
David <david.ddkino@gmail.com>
2022-12-11 15:45:45 +0200

    Update python.sh

--- python.sh
+++ python.sh
@@ -14,5 +14,14 @@ ls /usr/bin/python3*
 # install pip for specific version
 sudo apt-get install python3.8-dev python3.8-venv
  
+# check alternatives
+sudo update-alternatives --list python
+
+# setup alternatives
  
- 
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 2
+
+# change setup
+
+sudo update-alternatives --config python
